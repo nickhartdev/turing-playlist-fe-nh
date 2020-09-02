bc8db61b8576261e1ffa9cf99ad9129242262954
Nick Hart <nick.hart.des@gmail.com>
2020-09-02 10:24:31 -0500

    Populate homePage with song names

--- src/components/Songs/Songs.js
+++ src/components/Songs/Songs.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Songs = () => {
-  return (<h1>Songs</h1>)
+const Songs = (props) => {
+  return (
+    <div>
+      <h1>Songs</h1>
+      {props.songs.map(song => {
+        return <p>{song.songName}</p>
+      })}
+    </div>
+  )
 }
 
 export default Songs;
