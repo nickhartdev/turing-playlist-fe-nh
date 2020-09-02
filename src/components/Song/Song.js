217fc07ad0ca9cd8a516d8d46556fe98303aed16
Nick Hart <nick.hart.des@gmail.com>
2020-09-02 10:33:13 -0500

    Add some basic styling

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Song.css';
 
 const Song = (props) => {
   const song = props.song;
 
   return (
-    <div>
+    <div class="song-container">
       <p>{song.songName} by {song.artistName}</p>
       <a href={song.link}>Play</a>
     </div>
