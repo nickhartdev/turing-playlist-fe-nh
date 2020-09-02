368382185df7d390957e1822bc19ffa1c7486b6b
Nick Hart <nick.hart.des@gmail.com>
2020-09-02 11:19:27 -0500

    Add unit test for NewSongForm

--- src/components/NewSongForm/NewSongForm.test.js
+++ src/components/NewSongForm/NewSongForm.test.js
@@ -1,1 +1,20 @@
-import React from 'react;
+import React from 'react';
+import NewSongForm from './NewSongForm';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('NewSongForm', () => {
+  it('should render 3 fields and a button', () => {
+    render(<NewSongForm />);
+
+    const songNameField = screen.getByPlaceholderText('Song name');
+    const artistNameField = screen.getByPlaceholderText('Artist name');
+    const linkField = screen.getByPlaceholderText('Link');
+    const addSongButton = screen.getByRole('button');
+
+    expect(songNameField).toBeInTheDocument();
+    expect(artistNameField).toBeInTheDocument();
+    expect(linkField).toBeInTheDocument();
+    expect(addSongButton).toBeInTheDocument();
+  })
+})
