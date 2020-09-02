3d7630df4a32e5432f3c371a62baa81a4e8d0528
Nick Hart <nick.hart.des@gmail.com>
2020-09-02 12:47:48 -0500

    Add integration test for page render in App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,6 +8,47 @@ import '@testing-library/jest-dom';
 jest.mock('../../dataFetcher.js');
 
 describe('App', () => {
+  it('should display all songs from the server on load', async () => {
+    getAllSongs.mockResolvedValueOnce([
+      {
+        songName: "Swear",
+        artistName: "Casiopea",
+        link: "https://www.youtube.com/watch?v=6GEI3PpXEAo&t=1771s",
+        id: 1,
+      },
+      {
+        songName: "Autumn Leaves",
+        artistName: "Bill Evans Trio",
+        link: "https://www.youtube.com/watch?v=r-Z8KuwI7Gc",
+        id: 2,
+      },
+      {
+        songName: "Fair Weather",
+        artistName: "Art Farmer",
+        link: "https://www.youtube.com/watch?v=8-jfsUusSDQ",
+        id: 3,
+      },
+      {
+        songName: "Django",
+        artistName: "The Modern Jazz Quartet",
+        link: "https://www.youtube.com/watch?v=wXnkD7_5vqM",
+        id: 4,
+      },
+    ]);
+
+    render(<App />);
+
+    const song1 = await waitFor(() => screen.getByText('Swear by Casiopea'));
+    const song2 = await waitFor(() => screen.getByText('Autumn Leaves by Bill Evans Trio'));
+    const song3 = await waitFor(() => screen.getByText('Fair Weather by Art Farmer'));
+    const song4 = await waitFor(() => screen.getByText('Django by The Modern Jazz Quartet'));
+
+    expect(song1).toBeInTheDocument();
+    expect(song2).toBeInTheDocument();
+    expect(song3).toBeInTheDocument();
+    expect(song4).toBeInTheDocument();
+  })
+
   it('should allow the user to add a song', async () => {
     getAllSongs.mockResolvedValueOnce([
       {
